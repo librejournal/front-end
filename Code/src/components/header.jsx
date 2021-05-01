f30dcfb3e3083485ff0649d320898a970f9c9452
erolyagizaydin <32926423+ygzaydn@users.noreply.github.com>
2021-05-01 17:15:46 +0300

    asd

--- Code/src/components/header.jsx
+++ Code/src/components/header.jsx
@@ -3,54 +3,15 @@ import React from "react";
 import { Grid, Typography } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
-import { ReactComponent as Logo } from "../assets/logo/default-monochrome-white.svg";
-import { Link } from "react-router-dom";
-
-import { headerText } from "../constants";
-
 const useStyles = () => ({
-    headerGrid: {
-        height: "8vh",
-        backgroundColor: "#1687a7",
-        display: "flex",
-        justifyContent: "space-between",
-        padding: "2vh 5vw",
-        alignItems: "center",
-    },
-    headerMenuTexts: {
-        display: "flex",
-        justifyContent: "space-around",
-        color: "white",
-    },
-    headerMenuTextElements: {
-        cursor: "pointer",
-    },
-    headerLogo: {
-        width: "max(15vw,150px)",
-        padding: "1vh 0",
-        "& svg": {
-            maxHeight: "85px",
-        },
-    },
+  footerGrid: {
+    height: "8vh",
+    backgroundColor: "#1687a7",
+  },
 });
 
-const Header = ({ classes }) => {
-    return (
-        <Grid containter className={classes.headerGrid}>
-            <Grid item xs={2} className={classes.headerLogo}>
-                <Logo />
-            </Grid>
-            <Grid item xs={6} className={classes.headerMenuTexts}>
-                {headerText.menuItems.map((el) => (
-                    <Link to={el.path} style={{ textDecoration: "none" }}>
-                        <Typography className={classes.headerMenuTextElements}>
-                            {el.text}
-                        </Typography>
-                    </Link>
-                ))}
-            </Grid>
-        </Grid>
-    );
+const Footer = ({ classes }) => {
+  return <Grid containter className={classes.footerGrid} />;
 };
 
-export default withStyles(useStyles)(Header);
+export default withStyles(useStyles)(Footer);
--- Code/src/components/login/signinform.jsx
+++ Code/src/components/login/signinform.jsx
@@ -4,63 +4,55 @@ import { Grid, TextField, Typography, Button } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
 const useStyles = () => ({
-    signinContainer: {
-        width: "max(25vw,250px)",
-        padding: "2vh 2vw",
-        minHeight: "500px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-around",
-        textAlign: "center",
-        border: "2px solid #1687a7",
-    },
+  signinContainer: {
+    width: "max(25vw,250px)",
+    padding: "2vh 2vw",
+    minHeight: "500px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    textAlign: "center",
+    border: "2px solid #1687a7",
+  },
 });
 
 const SigninForm = ({ classes }) => {
-    return (
-        <Grid container justify="center" className={classes.signinContainer}>
-            <Typography color="primary" variant="h4">
-                Signin Form
-            </Typography>
-            <>
-                <TextField
-                    id="standard-basic"
-                    color="primary"
-                    label="Email"
-                    InputLabelProps={{
-                        style: { color: "#1687a7" },
-                    }}
-                />
-                <TextField
-                    id="standard-basic"
-                    color="primary"
-                    label="Password"
-                    InputLabelProps={{
-                        style: { color: "#1687a7" },
-                    }}
-                />
-                <TextField
-                    id="standard-basic"
-                    color="primary"
-                    label="First Name"
-                    InputLabelProps={{
-                        style: { color: "#1687a7" },
-                    }}
-                />
-                <TextField
-                    id="standard-basic"
-                    color="primary"
-                    label="Location"
-                    InputLabelProps={{
-                        style: { color: "#1687a7" },
-                    }}
-                />
-            </>
-            <Button variant="contained" color="primary">
-                SIGN IN
-            </Button>
-        </Grid>
-    );
+  return (
+    <Grid container justify="center" className={classes.signinContainer}>
+      <Typography color="primary" variant="h4">
+        Signin Form
+      </Typography>
+      <>
+        <TextField
+          id="standard-basic"
+          color="primary"
+          label="Email"
+          InputLabelProps={{
+            style: { color: "#1687a7" },
+          }}
+        />
+        <TextField
+          id="standard-basic"
+          color="primary"
+          label="Password"
+          InputLabelProps={{
+            style: { color: "#1687a7" },
+          }}
+        />
+        <TextField
+          id="standard-basic"
+          color="primary"
+          label="First Name"
+          InputLabelProps={{
+            style: { color: "#1687a7" },
+          }}
+        />
+      </>
+      <Button variant="contained" color="primary">
+        SIGN IN
+      </Button>
+    </Grid>
+  );
 };
 
 export default withStyles(useStyles)(SigninForm);
