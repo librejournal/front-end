ad1048d9fcf87432a75d193d475ed7ed2d64b250
erolyagizaydin <32926423+ygzaydn@users.noreply.github.com>
2021-05-27 23:34:08 +0300

    asd

--- Code/src/components/header.jsx
+++ Code/src/components/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Breadcrumbs } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
 import { ReactComponent as Logo } from "../assets/logo/default-monochrome-white.svg";
@@ -15,7 +15,7 @@ const useStyles = () => ({
     height: "8vh",
     backgroundColor: "#1687a7",
     display: "flex",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
     padding: "2vh 5vw",
     alignItems: "center",
   },
@@ -28,10 +28,9 @@ const useStyles = () => ({
     cursor: "pointer",
   },
   headerLogo: {
-    width: "max(15vw,150px)",
     padding: "1vh 0",
     "& svg": {
-      maxHeight: "85px",
+      maxHeight: "6vh",
     },
   },
 });
@@ -39,39 +38,41 @@ const useStyles = () => ({
 const Header = ({ classes, loggedUser }) => {
   return (
     <Grid containter className={classes.headerGrid}>
-      <Grid item xs={2} className={classes.headerLogo}>
+      <Grid item xs={4} md={2} className={classes.headerLogo}>
         <Logo />
       </Grid>
-      <Grid item xs={6} className={classes.headerMenuTexts}>
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <Typography className={classes.headerMenuTextElements}>
-            Home
-          </Typography>
-        </Link>
-        <Link
-          to={loggedUser.token === "" ? "/menu2" : "/stories"}
-          style={{ textDecoration: "none" }}
-        >
-          <Typography className={classes.headerMenuTextElements}>
-            {loggedUser.token === "" ? "Menu#2" : "Stories"}
-          </Typography>
-        </Link>
-        <Link
-          to={loggedUser.token === "" ? "/menu3" : "/createstory"}
-          style={{ textDecoration: "none" }}
-        >
-          <Typography className={classes.headerMenuTextElements}>
-            {loggedUser.token === "" ? "Menu#3" : "Create a story"}
-          </Typography>
-        </Link>
-        <Link
-          to={loggedUser.token === "" ? "/login" : "/account"}
-          style={{ textDecoration: "none" }}
-        >
-          <Typography className={classes.headerMenuTextElements}>
-            {loggedUser.token === "" ? "Login/Register" : "Account"}
-          </Typography>
-        </Link>
+      <Grid item xs={6} md={4} className={classes.headerMenuTexts}>
+        <Breadcrumbs separator="|" aria-label="breadcrumb">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <Typography className={classes.headerMenuTextElements}>
+              Home
+            </Typography>
+          </Link>
+          <Link
+            to={loggedUser.token === "" ? "/menu2" : "/stories"}
+            style={{ textDecoration: "none" }}
+          >
+            <Typography className={classes.headerMenuTextElements}>
+              {loggedUser.token === "" ? "Menu#2" : "Stories"}
+            </Typography>
+          </Link>
+          <Link
+            to={loggedUser.token === "" ? "/menu3" : "/createstory"}
+            style={{ textDecoration: "none" }}
+          >
+            <Typography className={classes.headerMenuTextElements}>
+              {loggedUser.token === "" ? "Menu#3" : "Create a story"}
+            </Typography>
+          </Link>
+          <Link
+            to={loggedUser.token === "" ? "/login" : "/account"}
+            style={{ textDecoration: "none" }}
+          >
+            <Typography className={classes.headerMenuTextElements}>
+              {loggedUser.token === "" ? "Login/Register" : "Account"}
+            </Typography>
+          </Link>
+        </Breadcrumbs>
       </Grid>
     </Grid>
   );
