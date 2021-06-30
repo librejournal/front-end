7f701578ea1f4b77ff20728e7253e69ac417141a
erolyagizaydin <32926423+ygzaydn@users.noreply.github.com>
2021-06-30 21:55:31 +0300

    asd

--- Code/src/index.js
+++ Code/src/index.js
@@ -8,16 +8,28 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.jsx";
 import { PersistGate } from "redux-persist/integration/react";
 import { ParallaxProvider } from "react-scroll-parallax";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://c2562845b4854a1694129c310d53fd02@o867507.ingest.sentry.io/5841579",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <ParallaxProvider>
-                <App />
-            </ParallaxProvider>
-        </PersistGate>
-    </Provider>,
-    document.getElementById("root")
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ParallaxProvider>
+        <App />
+      </ParallaxProvider>
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
