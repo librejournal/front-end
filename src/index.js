4ad52e98cf5ceaa376c0e7f3091912ff79a6d388
erolyagizaydin <32926423+ygzaydn@users.noreply.github.com>
2021-06-30 22:08:29 +0300

    design folder is removed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 
-Sentry.init({
+/*Sentry.init({
   dsn: "https://c2562845b4854a1694129c310d53fd02@o867507.ingest.sentry.io/5841579",
   integrations: [new Integrations.BrowserTracing()],
 
@@ -19,7 +19,7 @@ Sentry.init({
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
-});
+});*/
 
 ReactDOM.render(
   <Provider store={store}>
